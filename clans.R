0964dade4457a45d73c83759e4dce49458756dd4
Ekaterina Biryukova <kateabr@yandex.ru>
2019-03-22 19:50:27 +0300

    переписана функция шаффлинга, добавлен экспорт графиков в png

--- clans.R
+++ clans.R
@@ -1,9 +1,10 @@
 # - - - библиотеки - - -
 
-library(tidyverse)
+#library(tidyverse)
 library(GGally)
 library(dplyr)
 library(matrixStats)
+library(permute)
 
 # - - - функции - - -
 
@@ -74,15 +75,50 @@ dist_diff <- function(dt1, dt2, func, same_clan = TRUE){
 
 # перемешивание столбца таблицы
 # data: исходная таблица
-# domain: список всех значений, которые может принять элемент столбца
-shuffle <- function(data, domain){
+# return: таблица с перемешанными ярлыками кланов
+shuff <- function(data){
+  data.shuffled.A <- data[FALSE, ]
+  
+  # получение из таблицы списка людей вместе с кланом, к которому они принадлежат
   data %>%
-    mutate(ClanB = domain[sample(1:length(domain), nrow(data), replace = TRUE)]) %>%
-    return()
+    select(ClanA, IndA) %>%
+    distinct() -> clans_inds.A
+  colnames(clans_inds.A) <- c("Clan", "Ind")
+  
+  data %>%
+    select(ClanB, IndB) %>%
+    distinct() -> clans_inds.B
+  colnames(clans_inds.B) <- c("Clan", "Ind")
+  
+  clans_inds.A %>%
+    rbind(., clans_inds.B) %>%
+    distinct() -> clans_inds
+  
+  # случайная замена ярлыков кланов
+  clans_inds %>%
+    mutate(ClanShuffled = as.vector(clans_inds$Clan)[sample(1:nrow(clans_inds), nrow(clans_inds), replace = TRUE)]) -> clans_inds
+  
+  # обновление ярлыков для столбца ClanA
+  for (individual in clans_inds$Ind) {
+    data[data$IndA == individual, ] %>%
+      mutate(ClanA = clans_inds[clans_inds$Ind == individual, ]$ClanShuffled) %>%
+      rbind(data.shuffled.A, .) -> data.shuffled.A
+  }
+  
+  data.shuffled.B <- data[FALSE, ]
+  
+  # обновление ярлыков для столбца ClanB
+  for (individual in clans_inds$Ind) {
+    data.shuffled.A[data.shuffled.A$IndB == individual, ] %>%
+      mutate(ClanB = clans_inds[clans_inds$Ind == individual, ]$ClanShuffled) %>%
+      rbind(data.shuffled.B, .) -> data.shuffled.B
+  }
+  
+  return(data.shuffled.B)
 }
 
 # - - - основной код - - -
-
+setwd("D:\\Desktop\\villages_clans")
 data <- read.csv("D:\\Desktop\\Human Y-STR - Dist-for_Yulik - Human Y-STR - Dist(1).csv")
 data <- as.tibble(data)
 
@@ -93,50 +129,69 @@ data$VillageA %>%
   unique() -> villages # список всех деревень
 villages <- villages[villages != ""]
 
-for (village in villages){
-  data[data$VillageA == village, ]$ClanA %>%
-    unique() %>%
-    as.vector() -> clans # все кланы в деревне village
 
-  this_village <- data[data$VillageA == village & data$VillageB == village, ] # все люди из деревни village
+for(cnt in seq(1, 10)) {
+  for (village in villages){
+    data[data$VillageA == village, ]$ClanA %>%
+      unique() %>% as.vector() -> clans # все кланы в деревне village
   
-  res.same.mean = make_dist_df()
-  res.diff.mean = make_dist_df()
-  res.same.median = make_dist_df()
-  res.diff.median = make_dist_df()
-  res.same.sd = make_dist_df()
-  res.diff.sd = make_dist_df()
+    this_village <- data[data$VillageA == village & data$VillageB == village, ] # все люди из деревни village
+    
+    res.same.mean = make_dist_df()
+    res.diff.mean = make_dist_df()
+    res.same.median = make_dist_df()
+    res.diff.median = make_dist_df()
+    res.same.sd = make_dist_df()
+    res.diff.sd = make_dist_df()
+    
+    shuffled_diff_mean <- c()
+    
+    for (i in rep(0, 100)) {
+      this_village.shuffled <- shuff(this_village)
+      
+      # mean
+      res.same.mean <- bind_rows(res.same.mean, head(dist_diff(this_village, this_village.shuffled,
+                                                               func = colMeans, same_clan = TRUE), 4))
+      res.diff.mean <- bind_rows(res.diff.mean, head(dist_diff(this_village, this_village.shuffled,
+                                                                func = colMeans, same_clan = FALSE), 4))
   
-  for (i in rep(0, 1000)) {
-    this_village.shuffled <- shuffle(this_village, clans) # перемешивание столбца ClansB
-    
-    # mean
-    res.same.mean <- bind_rows(res.same.mean, head(dist_diff(this_village, this_village.shuffled,
-                                                             func = colMeans, same_clan = TRUE), 4))
-    res.diff.mean <- bind_rows(res.diff.mean, head(dist_diff(this_village, this_village.shuffled,
-                                                             func = colMeans, same_clan = FALSE), 4))
-    
-    # median
-    res.same.median <- bind_rows(res.same.median, head(dist_diff(this_village, this_village.shuffled,
-                                                             func = colMedians, same_clan = TRUE), 4))
-    res.diff.median <- bind_rows(res.diff.median, head(dist_diff(this_village, this_village.shuffled,
-                                                             func = colMedians, same_clan = FALSE), 4))
-    
-    # sd
-    res.same.sd <- bind_rows(res.same.sd, head(dist_diff(this_village, this_village.shuffled,
-                                                                 func = colSds, same_clan = TRUE), 4))
-    res.diff.sd <- bind_rows(res.diff.sd, head(dist_diff(this_village, this_village.shuffled,
-                                                                 func = colSds, same_clan = FALSE), 4)) 
+      # median
+      # res.same.median <- bind_rows(res.same.median, head(dist_diff(this_village, this_village.shuffled,
+      #                                                          func = colMedians, same_clan = TRUE), 4))
+      # res.diff.median <- bind_rows(res.diff.median, head(dist_diff(this_village, this_village.shuffled,
+      #                                                          func = colMedians, same_clan = FALSE), 4))
+      # 
+      # # sd
+      # res.same.sd <- bind_rows(res.same.sd, head(dist_diff(this_village, this_village.shuffled,
+      #                                                              func = colSds, same_clan = TRUE), 4))
+      # res.diff.sd <- bind_rows(res.diff.sd, head(dist_diff(this_village, this_village.shuffled,
+      #                                                              func = colSds, same_clan = FALSE), 4))
+      
+      shuffled_diff_mean <- c(shuffled_diff_mean, mean(res.diff.mean$Flat) - ifelse(is.na((x = mean(res.same.mean$Flat))[1]), 0, x))
+    }
+    break()
   }
   
-  break()
+  observed_diff <- mean(this_village[this_village$ClanA != this_village$ClanB, ]$Flat) -
+    ifelse(is.na((x = mean(this_village[this_village$ClanA == this_village$ClanB, ]$Flat))[1]), 0, x)
+  xlims <- t.test(shuffled_diff_mean)$conf.int[1:2]
+  if(observed_diff < xlims[1]) {
+    xlims[1] <- observed_diff - 1
+  }
+  if(observed_diff > xlims[2]) {
+    xlims[2] <- observed_diff + 1
+  }
+  
+  png(sprintf("%s_flat_mean_%i.png", village, cnt))
+  
+  hist(shuffled_diff_mean, main = "Flat: Mean", xlim = xlims)
+  
+  abline(v=observed_diff, col="green", lwd=2)
+  
+  abline(v=mean(shuffled_diff_mean), col="red", lwd=2)
+  
+  abline(v=t.test(shuffled_diff_mean)$conf.int[1], col="blue", lwd=2)
+  abline(v=t.test(shuffled_diff_mean)$conf.int[2], col="blue", lwd=2)
+  
+  dev.off()
 }
-
-hist(res.same.median$Loiselle - res.diff.median$Loiselle)
-
-shuffled_diff <- ifelse(is.na((x = median(res.same.median$Loiselle))[1]), 0, x) - median(res.diff.median$Loiselle)
-
-observed_diff <- ifelse(is.na((x = median(this_village[this_village$ClanA == this_village$ClanB, ]$Loiselle))[1]), 0, x) -
-  median(this_village[this_village$ClanA != this_village$ClanB, ]$Loiselle)
-abline(v=observed_diff, col="green", lwd=2)
-abline(v=shuffled_diff, col="red", lwd=2) 
